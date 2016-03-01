f2e04aced9551db903a8ea4da6f28a5d1984aade
jakubknejzlik <jakub.knejzlik@gmail.com>
2016-03-01 11:04:24 +0100

    style updates

--- example/index.js
+++ example/index.js
@@ -12,9 +12,8 @@ mod.config(function(AdminrMdLayoutProvider,AdminrMdLoginProvider,AdminrDataSourc
 
 
 mod.config(function(AdminrMdLayoutProvider) {
-    AdminrMdLayoutProvider.brandTitle = 'MdLayout example'
+    AdminrMdLayoutProvider.brandTitle = 'MdAdmin example'
     AdminrMdLayoutProvider.sidemenu.setHomePage('Dashboard', 'dashboard.html').setIcon('view-dashboard')
     AdminrMdLayoutProvider.sidemenu.addPage('users', 'Users', {url:'/users', templateUrl:'users.html'}).setIcon('account')
         .addPage('userDetail','User detail',{url:'/users/:id',templateUrl:'user-detail.html'})
-    AdminrMdLayoutProvider.sidemenu.addPage('table-panel', 'Table panel', {url:'/table-panel', templateUrl:'table-panel.html'}).setIcon('account-multiple')
 })
